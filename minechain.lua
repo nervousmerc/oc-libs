5c16ad337bddbd0746036eff8c805acc65996abf
Nervous <nervousmerc@gmail.com>
2017-08-01 21:19:47 +0500

    add actual hashing

--- minechain.lua
+++ minechain.lua
@@ -1,3 +1,7 @@
+local component = require('component')
+local data = assert(require('data')) -- requires a 'data' loot disk
+local datacard = assert(component.data) -- data card should be available
+
 ----------- MineChain object
 
 minechain = {}
@@ -18,13 +22,17 @@ function Block.new(index, previousHash, timestamp, data, hash)
   return o
 end 
 
+function Block.getGenesisBlock()
+  return Block.new(0,
+                   "0",
+                   0,
+                   "dixitque Deus: fiat lux, et facta est lux.",
+                   "361e406a85fcc54d358fa51b11d50e1517a5c2e97d2cd0b1163fd7e8a1f3367d")
+end
+
 function Block:calculateHash()
-  local hash = string.format("hash: %d %s %d %s",
-                              self.index, 
-                              self.previousHash, 
-                              self.timestamp, 
-                              self.data)
-  return hash
+  local state = self.index..self.previousHash..self.timestamp..self.data
+  return string.lower(data.toHex(datacard.sha256(state)))
 end
 
 minechain.Block = Block
