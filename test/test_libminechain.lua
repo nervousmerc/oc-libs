ab716a96667dc0e26189ac38f15dc7496ea66203
Nervous <nervousmerc@gmail.com>
2017-08-02 21:23:15 +0500

    add some tests

--- test/test_libminechain.lua
+++ test/test_libminechain.lua
@@ -1,4 +1,37 @@
-luaunit = require('luaunit')
+lu = require('luaunit')
+mc = require('libminechain')
+
+TestLib = {}
+function TestPrivate:testGenesisBlock()
+  local gb = mc.minechain[1]
+  lu.assertEquals(gb.index, 0)
+  lu.assertEquals(gb.previousHash, "0")
+  lu.assertEquals(gb.hash, "361e406a85fcc54d358fa51b11d50e1517a5c2e97d2cd0b1163fd7e8a1f3367d")
+  lu.assertEquals(gb.timestamp, 0)
+  lu.assertEquals(gb.data, "dixitque Deus: fiat lux, et facta est lux.")
+end
+
+function TestLib:testGetLatestBlock()
+end
+
+function TestLib:testCalculateHash()
+end
+
+function TestLib:testGenerateNextBlock()
+end
+
+function TestLib:testAddBlock()
+end
+
+function TestLib:testDumpBlock()
+end
+
+function TestLib:testDump()
+end
 
 
-os.exit( luaunit.LuaUnit.run()
+TestBlock = {}
+function TestBlock:testNew()
+end
+
+os.exit(luaunit.LuaUnit.run())
